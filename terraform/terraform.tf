20ca466242f7618b72a5d34ed92cddb4b7665b67
tejasmunjal3 <tejasmunjal@gmail.com>
2025-04-11 19:28:09 +0000

    initial coomit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "oak"   
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 
   required_version = ">= 1.6.3"
